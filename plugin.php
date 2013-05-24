dfdbd2193052ff8b01251f68100848a0eb0880eb
WPBoard <sap-dev@outlok.com>
2013-05-24 15:17:14 +0200

    check the plugin folder that this exist

--- plugin.php
+++ plugin.php
@@ -1,4 +1,19 @@
 <?php
+
+	/**
+		* @author WPBoard
+		* @copyright 2013 WPBoard
+		* @package com.wpboard.plugin
+		* @category Core
+		* @file plugin.php
+	*/
+
 	require 'base.php';
-	include('plugins/'.$_GET['plugin'].'/files/view.php');
+	$plugin = $_GET['plugin'];
+	
+	if(file_exists('plugins/'.$db->chars($plugin).'/files/view.php')) {
+		include('plugins/'.$db->chars($plugin).'/files/view.php');
+	} else {
+		message_box(template::getLanguage($config['selected_language'],'plugin_does_exists'));
+	}
 ?>
